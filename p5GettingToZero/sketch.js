98ecab0e4d1115702c82d019b42c4f3087de0c8f
dsbbbb <dsb510@gmail.com>
2016-06-09 12:17:52 -0400

    pushing everything day of as backup

--- p5GettingToZero/sketch.js
+++ p5GettingToZero/sketch.js
@@ -6,46 +6,68 @@
  * Move the sliders to control the R, G, B values of the background.
  */
 var slider;
-var total;
+var perCent;
 var totalNonViolent;
  var total = 52518;
-
+var daWords;
+var myFont;
+function preload() {
+  myFont = loadFont('assets/ETBembo-RomanLF.ttf');
+}
 // var totalRadius = d3.round(52518/300);
 function setup() {
- 
+  
+  // daWords = createP("IF WE RELEASED " + slider.value() + " PERCENT OF NON-VIOLENT OFFENDERS, IT WOULD REDUCE THE IMPRISONED NUMBER OF HUMANS TO " + peopleLeft, 50, 0, 500, 150);
+  // daWords.position(50,50);
   var totalNonViolent = round(total * .47);
-  console.log(totalNonViolent);
+  // console.log(totalNonViolent);
     // create canvas
   createCanvas(800, 800);
-  
-  // noLoop();
-  
-  
+
 
   // create sliders
   slider = createSlider(1, 99, 50);
   slider.position(180, 250);
    slider.style('width', '80px');
    slider.style('color', 'blue');
+  // slider.value().style('color', 'blue');
 
 }
 
 function draw() {
+  
   var totalNonViolent = round(total * .47);
   var r = .01 * slider.value();
+  // var perCent = slider.value();
+  var perCent = slider.value();
+
+
+  // var colorWordOne = createDiv(perCent);
+  
+// perCent.style('color', 'blue');
+  // perCent.style('color','blue');
   // console.log(r);
   // console.log(totalNonViolent);
   var zero = d3.format(",");
   var updatedNonViolent= totalNonViolent * r;
 var updatedTotal = round(total - updatedNonViolent);
   var peopleLeft = zero(updatedTotal);
-  background(33, 33, 33);
-  textSize(22);
-  fill(3, 169, 244);
-  textFont("font-family: 'Fira Sans', sans-serif");
-  fill(255, 255, 255);
-  text("If we released " + slider.value() + " percent of non violent offenders, it would reduce the imprisoned number of humans to " + peopleLeft, 50, 0, 500, 150);
-  fill(255, 160, 0)
+  background(255, 255, 248);
+fill('#111');
+  textFont(myFont);
+  textSize(24);
+text("IF WE RELEASED ", 50, 0, 500, 150);
+fill('#C34242');
+text(perCent + " PERCENT ", 257, 0, 500, 150);
+// + perCent + " PERCENT 50, 0, 500, 150);
+//   OF NON-VIOLENT OFFENDERS, IT WOULD REDUCE THE IMPRISONED NUMBER OF HUMANS TO " + peopleLeft, 
+ 
+  // text("IF WE RELEASED " + slider.value() + " PERCENT OF NON-VIOLENT OFFENDERS, IT WOULD REDUCE THE IMPRISONED NUMBER OF HUMANS TO " + peopleLeft, 50, 0, 500, 150);
+   fill(254, 252, 162);
+  ellipse(350, 350, 350 , 350 );
+  fill(255, 160, 0);
+ strokeWeight(4);
+stroke(51);
   ellipse(350, 350, updatedTotal/150 , updatedTotal/150 );
   // console.log(updatedTotal);
   // console.log(updatedTotal);
